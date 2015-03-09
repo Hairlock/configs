ea0c84ff4fcfd6e029e6938859ff04e06f268129
Yannick Sealy <ysealy@travelrepublic.co.uk>
2015-03-09 16:36:41 +0000

    emacs updates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -130,3 +130,37 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
+
+(defun my-csharp-mode-hook ()
+  ;; enable the stuff you want for C# here
+  (electric-pair-mode 1))
+(add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
+
+(require 'tree-mode)
+(require 'windata)
+(require 'dirtree)
+(autoload 'dirtree "dirtree" "Add directory to tree view")
+;;(nav-disable-overeager-window-splitting)
+;; Optional: set up a quick key to toggle nav
+;;(global-set-key [f8] 'nav-toggle)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#eaeaea" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#000000"))
+ '(coffee-tab-width 2)
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "030bed79e98026124afd4ef8038ba7fe064314baf18b58759a5c92b91ec872fb" "23cf1bbd82721df1785aa1a10f742e555d6ea41921b65fab0345947bdd56c3f8" "f791ab63f888301f6eee69e3e262e1fa92fe705a1ef11bfdd49a2fac461bbe08" "b2231b396c332cf82d255f6a882687cb3c97ac36ee4b918713883a8f148af966" "e9f642ee0dbd5638e40390b8b8eded9743f1426ad1390e7b2e5d3fa04efa2969" default)))
+ '(fci-rule-color "#2a2a2a"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
