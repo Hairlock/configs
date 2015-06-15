41c0deb0bb14ad3b609980da9d9a3134c4b5c9c2
yannick <yannick.sealy08@gmail.com>
2015-06-15 19:51:17 +0100

    emacs

---
+++ .emacs.d/elpa/clojure-cheatsheet-20140316.1520/clojure-cheatsheet-autoloads.el
@@ -0,0 +1,28 @@
+;;; clojure-cheatsheet-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "clojure-cheatsheet" "../../../../../.emacs.d/elpa/clojure-cheatsheet-20140316.1520/clojure-cheatsheet.el"
+;;;;;;  "242057e86be1864aa9f65d2b0cb03bfb")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/clojure-cheatsheet-20140316.1520/clojure-cheatsheet.el
+
+(autoload 'clojure-cheatsheet "clojure-cheatsheet" "\
+Use helm to show a Clojure cheatsheet.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/clojure-cheatsheet-20140316.1520/clojure-cheatsheet-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/clojure-cheatsheet-20140316.1520/clojure-cheatsheet.el")
+;;;;;;  (21885 57545 892000 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; clojure-cheatsheet-autoloads.el ends here
