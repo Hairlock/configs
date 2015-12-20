3a1fc053c41a844096dc3be108de992ba36ce4dd
yannick <yannick.sealy08@gmail.com>
2015-12-20 16:52:23 +0000

    sublime

--- .emacs.d/customizations/editing.el
+++ .emacs.d/customizations/editing.el
@@ -100,3 +100,58 @@
 
 (global-set-key [home] 'smart-beginning-of-line)
 (global-set-key "\C-a" 'smart-beginning-of-line)
+
+(defun get-point (symbol &optional arg)
+      "get the point"
+      (funcall symbol arg)
+      (point)
+     )
+     
+     (defun copy-thing (begin-of-thing end-of-thing &optional arg)
+       "copy thing between beg & end into kill ring"
+        (save-excursion
+          (let ((beg (get-point begin-of-thing 1))
+         	 (end (get-point end-of-thing arg)))
+            (copy-region-as-kill beg end)))
+     )
+     
+     (defun paste-to-mark(&optional arg)
+       "Paste things to mark, or to the prompt in shell-mode"
+       (let ((pasteMe 
+     	 (lambda()
+     	   (if (string= "shell-mode" major-mode)
+     	     (progn (comint-next-prompt 25535) (yank))
+     	   (progn (goto-char (mark)) (yank) )))))
+     	(if arg
+     	    (if (= arg 1)
+     		nil
+     	      (funcall pasteMe))
+     	  (funcall pasteMe))
+     	))
+		
+(defun copy-word (&optional arg)
+      "Copy words at point into kill-ring"
+       (interactive "P")
+       (copy-thing 'backward-word 'forward-word arg)
+       ;;(paste-to-mark arg)
+     )
+
+(global-set-key (kbd "C-c w")         (quote copy-word))
+
+ (defun copy-line (&optional arg)
+      "Save current line into Kill-Ring without mark the line "
+       (interactive "P")
+       (copy-thing 'beginning-of-line 'end-of-line arg)
+       (paste-to-mark arg)
+     )
+
+(global-set-key (kbd "C-c l")         (quote copy-line))
+
+(defun copy-paragraph (&optional arg)
+      "Copy paragraphes at point"
+       (interactive "P")
+       (copy-thing 'backward-paragraph 'forward-paragraph arg)
+       (paste-to-mark arg)
+     )
+	 
+(global-set-key (kbd "C-c p")         (quote copy-paragraph))
